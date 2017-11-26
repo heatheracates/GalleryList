ffc04d47edc633a358fe777f5fc8127ee2340795
Heather Cates <cates.heather.a@gmail.com>
2017-11-25 23:13:23 -0800

    added basic views and default image

---
+++ GalleryList/AddPictureViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddPictureViewController.swift
+//  GalleryList
+//
+//  Created by Heather Cates on 11/25/17.
+//  Copyright © 2017 Heather Cates. All rights reserved.
+//
+
+import UIKit
+
+class AddPictureViewController: UIViewController {
+
+    @IBOutlet weak var galleryImageView: UIImageView!
+    @IBOutlet weak var galleryImageTextFeild: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func photosButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addToGalleryTapped(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
